626b0c705972ec0b14e192cecee3382e303cc899
OneGithubforMe <32235044+OneGithubforMe@users.noreply.github.com>
2021-04-22 19:52:57 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-        .register('sw_cached_sites.js')
+        .register('assets/js/sw_cached_sites.js')
         .then(reg => console.log("Service Worker: registered"))
         .catch(err => console.log(`Service Worker: Error: ${err}`))
     })
