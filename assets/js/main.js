59ff4a95934507954a87f03632610a2dd300edcd
OneGithubforMe <32235044+OneGithubforMe@users.noreply.github.com>
2021-04-22 20:30:19 +0530

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-if('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-        .register('assets/js/sw_cached_sites.js')
-        .then(reg => console.log("Service Worker: registered"))
-        .catch(err => console.log(`Service Worker: Error: ${err}`))
-    })
-}
